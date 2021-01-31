2300fb5982b86b2dc7b8172ddfabcd3a9c117e1c
Agustin Allende <agustin_allende@hotmail.com>
2021-01-31 15:07:54 -0300

    correcciones

--- Assets/Scripts/Player/PlayerMentalHealthController.cs
+++ Assets/Scripts/Player/PlayerMentalHealthController.cs
@@ -6,10 +6,35 @@ public class PlayerMentalHealthController : MonoBehaviour
 {
 
     public PlayerMentalHealthEnum MentalState;
-   
+    public RuntimeAnimatorController AnimPlayerCuerdo;
+    public RuntimeAnimatorController AnimPlayerNeutro;
+    public RuntimeAnimatorController AnimPlayerLoco;
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
     public void ChangeMentalHealth(PlayerMentalHealthEnum newMentalHealth)
     {
         MentalState = newMentalHealth;
+
+        switch (MentalState)
+        {
+            case PlayerMentalHealthEnum.Demente:
+                anim.runtimeAnimatorController = AnimPlayerLoco;
+                break;
+            case PlayerMentalHealthEnum.Cuerdo:
+                anim.runtimeAnimatorController = AnimPlayerCuerdo;
+                break;
+            case PlayerMentalHealthEnum.Neutro:
+                anim.runtimeAnimatorController = AnimPlayerNeutro;
+                break;
+            default:
+                break;
+        }
+
     }
 
 
